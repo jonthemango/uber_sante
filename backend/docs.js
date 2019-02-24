4611dcdad559c451e2730d530e49e48d0a517f83
Unknown <jonathan.mongeau@mail.concordia.ca>
2019-02-24 16:32:56 -0500

    Added URI links to self documenter.
    Co-authored-by: ramzouza <ramezzaid@outlook.com>

--- backend/docs.js
+++ backend/docs.js
@@ -1,6 +1,6 @@
 
 
-const document_routers = (routers_json, top_level_route) => {
+const document_routers = (routers_json, top_level_route, port) => {
   const api_package = {}
   routers = []
   router_names = []
@@ -11,25 +11,28 @@ const document_routers = (routers_json, top_level_route) => {
 
   for(let i=0; i<routers.length; i++){
     router = routers[i];
-    api_package[router_names[i]] = document_router(router, top_level_route)
+    api_package[router_names[i]] = document_router(router, top_level_route, port)
   }
   return api_package
 }
 
 
 
-const document_router = (router, top_level_route) => {
+const document_router = (router, top_level_route, port) => {
   let route_obj;
   let path;
   const api_package = []
   for(let i=0; i<router.stack.length; i++){
+
       route_obj = router.stack[i].route;
+      console.log(route_obj);
       method = route_obj.stack[0].handle
       method_definition = route_obj.stack[0].handle.toString()
       method_name = route_obj.stack[0].name;
       path = top_level_route + route_obj.path;
+      uri = "http://localhost:" + port + path
       method = route_obj.stack[0].method;
-      api_package.push({path, method, method_name, description:""});
+      api_package.push({uri, path, method, method_name, description:""});
   }
   return api_package
 }
