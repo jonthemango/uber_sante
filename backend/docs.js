e675d300ef22e8f20c79f1e485ebbb3a5a0b0779
Unknown <jonthemango@gmail.com>
2019-02-24 13:58:19 -0500

    Revert "Set up controller for Appointments and added other models"
    This reverts commit e07b4d1696ead5f2d3c7f19da0c0b80bf2c6f01d.

--- backend/docs.js
+++ backend/docs.js
@@ -1,4 +1,5 @@
 
+
 const document_routers = (routers_json, top_level_route) => {
   const api_package = {}
   routers = []
@@ -24,7 +25,7 @@ const document_router = (router, top_level_route) => {
   for(let i=0; i<router.stack.length; i++){
       route_obj = router.stack[i].route;
       method = route_obj.stack[0].handle
-      method_definition = route_obj.stack[0].handle.toString();
+      method_definition = route_obj.stack[0].handle.toString()
       method_name = route_obj.stack[0].name;
       path = top_level_route + route_obj.path;
       method = route_obj.stack[0].method;
