9123034b0bbe3460b0b26b5aaa3f826c3596d57a
Yann Cedric <ngadeuyann@yahoo.fr>
2019-02-26 17:13:45 -0500

    Added main home page
    Co-authored-by: Eric Kokmanian <erickokmanian1997@gmail.com>
    Co-authored-by: skanderbm123 <skander96@hotmail.com>
    Co-authored-by: Yann Cedric <ngadeuyann@yahoo.fr>

--- frontend/src/App.router.js
+++ frontend/src/App.router.js
@@ -1,13 +1,13 @@
 import React, { Component } from 'react'
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
-import Login from './Login'
+import Home from './Home'
 
 class App extends Component {
   render() {
     return (
       <Router>
         <Switch>
-          <Route exact path="/" component={Login}  />
+          <Route exact path="/" component={Home}  />
         </Switch>
       </Router>
     );
