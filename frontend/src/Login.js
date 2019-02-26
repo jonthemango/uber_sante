acf0fd5d9fef6fffef92b332164b806d7ce0c470
Unknown <skander96@hotmail.com>
2019-02-26 12:57:28 -0500

    create Login Class

---
+++ frontend/src/Login.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import cookie from 'react-cookies'
+
+class Login extends Component {
+    constructor(){
+        super()
+        
+        
+      }
+
+
+
+
+    
+  render() {
+
+
+
+
+  }
+}
+
+
+export default Login;
