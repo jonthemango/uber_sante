89b8d2ec43bc0692de744c460eae7d85dcb74b00
Ribal Aladeeb <aladeeb.r@hotmail.com>
2019-02-27 09:41:02 -0500

    Added an http endpoint tester to help debugging
    Co-authored-by: ramzouza <ramezzaid@outlook.com>
    Co-authored-by: jonthemango <jonathan.mongeau@mail.concordia.ca>
    Co-authored-by: Sibachir Ahmed-Yanis <ahmed.yanis.sibachir@gmail.com>Co-authored-by: ramzouza <ramezzaid@outlook.com>

--- backend/apiTester.js
+++ backend/apiTester.js
@@ -3,22 +3,55 @@ const axios = require('axios')
 
 // Optionally the request above could also be done as
 
-axios.put('http://localhost:5001/api/patients/5c74ac49765e71617070348a',{
-  healthCardNB:'whatever',
-  birthDay:"1 january 2019",
-  gender:"non-binary",
-  phoneNumber: 5145443322,
-  physicalAddress:"cavendish",
-  email :'ribal@aladeeb.com',
-  passwordHash :'secret'
+// axios.post('http://localhost:5001/api/doctors/5c75c3afad74522637fc2561/availability', {
+//   availability: {
+//     "monday": [false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false],
+//     "tuesday": [false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false],
+//     "wednesday": [false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false],
+//     "thursday": [false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false],
+//     "friday": [false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false]
+//   }
+// })
+//   .then(function (response) {
+//     console.log(JSON.stringify(response.data, undefined, 2));
+//   })
+//   .catch(function (error) {
+//     console.log(error);
+//   });
+
+const Doctor = require('./models/Doctors')
+// console.log(Doctor);
+const doctor = new Doctor({
+  permit: 'a random permit',
+  firstname: 'ribal',
+  lastname: 'aladeeb',
+  city: 'montreal',
 })
-  .then(function (response) {
-    console.log(JSON.stringify(response.data, undefined, 2));
-  })
+axios.post('http://localhost:5001/api/patients', doctor)
+  .then(function (response) { console.log(JSON.stringify(response.data, undefined, 2)); })
   .catch(function (error) {
     console.log(error);
   });
 
+
+
+
+  // const noWeeks = 3;
+  // let dayObj;
+  // let day;
+  // let dates = {
+  //   "monday":[],
+  //   "tuesday":[],
+  //   "wednesday":[],
+  //   "thursday":[],
+  //   "friday":[]
+  // };
+  // for (let i=0; i<noWeeks*7; i++){
+  //   dayObj = moment().add(i,'days');
+  //   day = dayObj.format('dddd').toLowerCase()
+  //   if (day == 'saturday' || day == 'sunday'){ continue; }
+  //     dates[day].push(dayObj.format('L'))
+  // }
 
 
 
