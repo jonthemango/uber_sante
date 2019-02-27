3c85a6d539e6675bb31f5e7cbb57365035a2986d
Eric Kokmanian <erickokmanian1997@gmail.com>
2019-02-26 19:33:48 -0500

    Added login route from home page

--- frontend/src/App.router.js
+++ frontend/src/App.router.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import Home from './Home'
+import Login from './Login'
 
 class App extends Component {
   render() {
@@ -8,6 +9,7 @@ class App extends Component {
       <Router>
         <Switch>
           <Route exact path="/" component={Home}  />
+          <Route exact path="/login" component={Login}  />
         </Switch>
       </Router>
     );
